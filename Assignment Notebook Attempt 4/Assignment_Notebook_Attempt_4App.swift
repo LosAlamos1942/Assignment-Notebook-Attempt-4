75e2badb03f09fa4901e50c009fdf1034d24ccae
Nerto Kageh <kagehnerto@yahoo.com>
2022-01-28 10:33:07 -0600

    Initial Commit

---
+++ Assignment Notebook Attempt 4/Assignment_Notebook_Attempt_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_Notebook_Attempt_4App.swift
+//  Assignment Notebook Attempt 4
+//
+//  Created by Student on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_Notebook_Attempt_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment Notebook Attempt 4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment Notebook Attempt 4
+//
+//  Created by Student on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
